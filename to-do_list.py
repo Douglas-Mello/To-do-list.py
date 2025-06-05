4886bb2e0a532bbc877259994743b55f512f80eb
Douglas Mello <douglasaugusto.mello@gmail.com>
2025-06-05 15:19:10 -0300

    Primeiros passos

--- to-do_list.py
+++ to-do_list.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+tarefa = list()
+while True:
+    print("===  Menu de opções:  ====")
+    print("1.Adicionar Tarefa \n2.Ver tarefas\n3.Conclui uma tarefa\n4.Remover tarefa\n5.Sair")
+    opc=int(input('Escolha uma opção: '))
+    if(opc not in range(1,6)):
+        print()
+        print("\n     ERRO!!!  \n     opção inválida")
+        print()
+        continue
